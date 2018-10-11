ca1e43fdff2b49cd696694a2d9e395438bfc3aad
da crands <davecrands@gmail.com>
2018-10-10 20:12:08 -0400

    Added jwt auth to other routes

--- app/routes.py
+++ app/routes.py
@@ -17,47 +17,30 @@ def register():
     db.session.add(user)
     db.session.commit()
 
-    print(request.form['username'])
-
     return 'register'
-
 
 @app.route('/login', methods=['POST'])
 def login():    
-    jsonData = json.loads(request.data.decode('utf-8')) 
-    print('test1')   
     if current_user.is_authenticated:
-        return redirect('/api/popular')    
-    print('test2')
+        return redirect('/api/popular')
+        
+    jsonData = json.loads(request.data.decode('utf-8'))         
     user = User.query.filter_by(username=jsonData['username']).first()
-    print(user)
     if user is None or not user.check_password(jsonData['password']):
         print('invalid')
         return 'invalid'
-
-    print('test3')
     access_token = create_access_token(identity=user.username)
-    print('test3')
-    # jwtData = jsonify(access_token=access_token)
-    # print(jwtData)
     jwtData = json.dumps({"access_token" :access_token})
-    print(jwtData)
+
     return jwtData
-
-    # print(type(request.data.decode('utf-8')))
-    # jsonData = json.loads(request.data.decode('utf-8'))
-    # print(type(jsonData))
-    # return 'login'
-
 
 @app.route('/logout')
 def logout():
     logout_user()
+
     return 'logged out'
 
-
 @app.route('/api/popular')
-# @login_required
 @jwt_required
 def popular():
     res = requests.get(
@@ -65,14 +48,16 @@ def popular():
         .format(app.config['API_KEY']))
     if res.status_code != 200:
         errData = {'status': res.status_code, 'error': 'There was an error'}
+
         return jsonify(errData), res.status_code
 
     popularData = jsonify(res.json())
+
     return popularData
 
 
 @app.route('/api/best')
-@login_required
+@jwt_required
 def best():
     res = requests.get(
         'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-nonfiction.json?api-key={0}'
@@ -85,12 +70,13 @@ def best():
     return bestData
 
 
-@app.route('/api/archives/<month>/<year>')
-@login_required
+@app.route('/api/archives/<year>/<month>')
+@jwt_required
 def archives(month, year):
+    print(month)
     res = requests.get(
         'https://api.nytimes.com/svc/archive/v1/{0}/{1}.json?api-key={2}'.
-        format(month, year, app.config['API_KEY']))
+        format(year, month, app.config['API_KEY']))
     if res.status_code != 200:
         errData = {'status': res.status_code, 'error': 'There was an error'}
         return jsonify(errData), res.status_code
